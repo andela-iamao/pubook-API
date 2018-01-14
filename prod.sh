6d897be2a4fd4b1be3e3df3118aad48b5ceb9118
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2018-01-14 03:10:29 +0100

    fix production config issues and move to using docker

--- prod.sh
+++ prod.sh
@@ -2,4 +2,9 @@
 
 export GIN_MODE=release
 
+go get github.com/emicklei/forest
+go get github.com/lib/pq
+go get github.com/gin-gonic/gin
+go get github.com/astaxie/beego/orm
+
 go build -o bin/application main.go
